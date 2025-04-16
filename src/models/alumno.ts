eab941f91d5aa45211743ea360ea2122801004a3
Andres <andreseg65@gmail.com>
2025-04-16 19:25:04 -0300

    En este commit modifique algunos comentarios para que estén mas claros

--- src/models/alumno.ts
+++ src/models/alumno.ts
@@ -1,12 +1,15 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
-const alumnoSchema = new Schema({
-  nombre: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  edad: { type: Number },
-  cursos: [{ type: Schema.Types.ObjectId, ref: 'Curso' }] // Relación con cursos
-}, {
-  timestamps: true
-});
+const alumnoSchema = new Schema(
+  {
+    nombre: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    edad: { type: Number },
+    cursos: [{ type: Schema.Types.ObjectId, ref: "Curso" }], // Acá relaciono al alumno con los cursos
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const Alumno = model('Alumno', alumnoSchema);
+export const Alumno = model("Alumno", alumnoSchema);
--- src/models/profesor.ts
+++ src/models/profesor.ts
@@ -1,12 +1,15 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
-const profesorSchema = new Schema({
-  nombre: { type: String, required: true },
-  especialidad: { type: String },
-  email: { type: String, required: true, unique: true },
-  cursos: [{ type: Schema.Types.ObjectId, ref: 'Curso' }] // Relación con cursos
-}, {
-  timestamps: true
-});
+const profesorSchema = new Schema(
+  {
+    nombre: { type: String, required: true },
+    especialidad: { type: String },
+    email: { type: String, required: true, unique: true },
+    cursos: [{ type: Schema.Types.ObjectId, ref: "Curso" }], // Acá relaciono al profesor con los cursos
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const Profesor = model('Profesor', profesorSchema);
+export const Profesor = model("Profesor", profesorSchema);
